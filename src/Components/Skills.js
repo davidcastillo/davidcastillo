62549c74984f01c378e991094e03420850a520c2
David Castillo <castillorganization@gmail.com>
2022-03-10 21:37:20 +0100

    skills updated

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -9,55 +9,7 @@ import aws from './assets/aws.png';
 import api from './assets/API.png';
 import {Table} from 'react-bootstrap';
 import '../App.css';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
 
-import { Bar } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-  
-  export const options = {
-    indexAxis: 'y',
-    elements: {
-      bar: {
-        borderWidth: 4,
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        display: false, 
-      }
-    },
-  };
-  
-  const labels = ['HTML5', 'CSS', 'Javascript', 'APIs', 'React JS', 'GIT', 'AWS', 'Firebase'];
-  
-  export const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Selflearn',
-        data: [1,2,3,1,2,3,1],
-        backgroundColor: 'rgba(31, 158, 211)',
-      }
-    ],
-  };
-  
 
 const Skills = () => {
     return ( 
